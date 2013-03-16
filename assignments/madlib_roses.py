4a9129e290231bb66b8c36553f5b57d3770c9624
juan <juan.musleh@gmail.com>
2013-03-16 12:24:24 -0400

    Madlib updates

---
+++ assignments/madlib_roses.py
@@ -0,0 +1,13 @@
+#Code is from p2pu
+#https://p2pu.org/en/groups/playing-madlibs-with-python-building-your-first-game/content/build-a-simple-madlib-using-wing-ide/
+
+adjective1 = raw_input("Tell me an adjective, and click enter. ")
+noun1 = raw_input("Tell me a noun (plural), and click enter. ")
+noun2 = raw_input("Tell me another noun, and click enter. ")
+adjective2 = raw_input("Tell me an another adjective, and click enter. ")
+
+print "Roses are " + adjective1
+print noun1 + " are blue"
+print noun2 + " is " + adjective2
+print "And so are you!"
+
